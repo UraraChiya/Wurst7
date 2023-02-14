fcc68c8af87f416730764dd649d2e3e72739ec36
UraraChiya <284354572@qq.com>
2023-02-14 23:16:58 +0800

    fix NoteBotHack

--- src/main/java/net/wurstclient/util/NoteBot.java
+++ src/main/java/net/wurstclient/util/NoteBot.java
@@ -13,7 +13,6 @@ import net.minecraft.block.enums.Instrument;
 import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.registry.Registries;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.MathHelper;
