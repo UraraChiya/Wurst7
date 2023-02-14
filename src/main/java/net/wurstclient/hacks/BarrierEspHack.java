8e073dcc81809d70508be94b710ed3c8c6d88b69
UraraChiya <284354572@qq.com>
2023-02-14 23:52:22 +0800

    new hack: Barrier Esp
    It's only works when the Particles is set to all

---
+++ src/main/java/net/wurstclient/hacks/BarrierEspHack.java
@@ -0,0 +1,13 @@
+package net.wurstclient.hacks;
+
+import net.wurstclient.hack.Hack;
+import net.wurstclient.SearchTags;
+
+@SearchTags({"barrieresp", "BarrierESP", "barrier esp"})
+public class BarrierEspHack extends Hack {
+
+	public BarrierEspHack() {
+		super("BarrierESP");
+	}
+
+}
